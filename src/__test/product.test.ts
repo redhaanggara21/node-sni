0433e7c220ef8a416b79a167674b6700c654ba99
redhaanggara21 <redhabayuanggara@gmail.com>
2023-03-05 17:34:16 +0700

    update nodes

--- src/__test/product.test.ts
+++ src/__test/product.test.ts
@@ -25,49 +25,35 @@ describe('product', () => {
             await mongoose.disconnect()
             await mongoose.connection.close()
         });
-        
-        // describe('get all product', () => {
-        //     describe('given the return 200 and detail product data', async() => {
-        //         const { statusCode, body } = await supertest(app).get(`/product`)
-        //         expect(statusCode).toBe(200);
-        //     });
-        // });
 
-        describe('get detail product', async() => {
+        describe('get detail product',() => {
 
-            describe('given the product does not exist', () => {
-
-                it('should return 404, and empty data', async () => {
-                    const productId = 'PRODUCT_123';
-                    const { statusCode } = await supertest(app).get(`/product/${productId}`).expect(404);
-                    await supertest(app).get(`/product/${productId}`).expect(404)
-                });
-
-                it('should return 200, and detail product data', async () => {   
-                    const productId1 =  await addProductToDB(productPayload); 
-                    const { statusCode, body } = await supertest(app).get(`/product/${productId1}`);
-                    expect(statusCode).toBe(200);
-                    expect(body.data.name).toBe("Your Tshirt Size Large Up");
-                });
-            })  
-        })
-
-        describe('create product', async() => {
-            describe('if user not authorized', async() => {
-                it('should return 403, request forbiden', async () => {
-                    const { statusCode } = await supertest(app).post('/product').send(productPayload);
-                    expect(statusCode).toBe(403);
-                });
+            it('should return 404, and empty data', async () => {
+                const productId = 'PRODUCT_123';
+                const { statusCode } = await supertest(app).get(`/product/${productId}`).expect(404);
+                await supertest(app).get(`/product/${productId}`).expect(404)
             });
 
-            describe('if user authorized', async() => {
-                it('should return 201, success create product', async () => {
-                    const { statusCode, body } = await supertest(app).post('/product').send(productPayload);
-                    console.log(body.data);
-                    expect(statusCode).toBe(201);
-                });
+            it('should return 200, and detail product data', async () => {   
+                const productId1 =  await addProductToDB(productPayload); 
+                const { statusCode, body } = await supertest(app).get(`/product/${productId1}`);
+                expect(statusCode).toBe(200);
+                expect(body.data.name).toBe("Your Tshirt Size Large Up");
             });
         });
 
+        describe('create product', () => {
+            it('should return 403, request forbiden', async() => {
+                const { statusCode } = await supertest(app).post('/product').send(productPayload);
+                expect(statusCode).toBe(403);
+            });
+        });
+
+        // describe('if user authorized', () => {
+        //     it('should return 201, success create product', async () => {
+        //         const { statusCode, body } = await supertest(app).post('/product').send(productPayload);
+        //         expect(statusCode).toBe(201);
+        //     });
+        // });
     }
 )
